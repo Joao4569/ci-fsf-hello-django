8ab59667e3c7aa3f050b6691e6fe6e9aff9e15fb
Joao4569 <johnnyrod4569@gmail.com>
2022-06-07 12:50:54 +0000

    Update Heroku's DATABASE_URL

--- __init__.py
+++ __init__.py
@@ -2,9 +2,13 @@
 docstring
 """
 import os
+import re
 
 
 if os.environ.get("DEVELOPMENT") == "True":
     os.environ.get("DB_URL")
 else:
-    os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    # app.config["..."] = uri # heroku
