d92266a8ef4ada87caf1a6e6a6da80b2afbf3a93
Joao4569 <johnnyrod4569@gmail.com>
2022-06-07 14:16:23 +0000

    hide SECRET_KEY - heroku warning

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^_dr#q)dt#7*h287_3$e*1+ze*mch&a&3lnos_00&25ha-b+c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
